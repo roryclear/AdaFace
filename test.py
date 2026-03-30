85baa5722ac0160441a1dba2db30c391da92adc1
roryclear <roryclear.rc@gmail.com>
2026-03-30 17:54:07 +0100

    move stuff to file

--- test.py
+++ test.py
@@ -1,7 +1,28 @@
-from face_alignment import align
-from inference import load_pretrained_model, to_input
 import cv2
 import numpy as np
+import torch
+import net
+
+adaface_models = {
+    'ir_50':"pretrained/adaface_ir50_ms1mv2.ckpt",
+}
+
+def to_input(pil_rgb_image):
+    np_img = np.array(pil_rgb_image)
+    brg_img = ((np_img[:,:,::-1] / 255.) - 0.5) / 0.5
+    tensor = torch.tensor([brg_img.transpose(2,0,1)]).float()
+    return tensor
+
+def load_pretrained_model(architecture='ir_50'):
+    # load model and pretrained statedict
+    assert architecture in adaface_models.keys()
+    model = net.build_model(architecture)
+    statedict = torch.load(adaface_models[architecture],  map_location="cpu", weights_only=False)['state_dict']
+    model_statedict = {key[6:]:val for key, val in statedict.items() if key.startswith('model.')}
+    model.load_state_dict(model_statedict)
+    model.eval()
+    return model
+
 
 model = load_pretrained_model('ir_50')
 
