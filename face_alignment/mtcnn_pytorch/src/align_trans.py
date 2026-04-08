1002e34d0c7fec69ebd51e1c07f45a998d78f6d8
roryclear <roryclear.rc@gmail.com>
2026-04-08 11:31:54 +0100

    clean

--- face_alignment/mtcnn_pytorch/src/align_trans.py
+++ face_alignment/mtcnn_pytorch/src/align_trans.py
@@ -9,7 +9,7 @@ import cv2
 # from scipy.linalg import lstsq
 # from scipy.ndimage import geometric_transform  # , map_coordinates
 
-from mtcnn_pytorch.src.matlab_cp2tform import get_similarity_transform_for_cv2, get_similarity_transform, cvt_tform_mat_for_cv2
+from mtcnn_pytorch.src.matlab_cp2tform import findSimilarity, get_similarity_transform, cvt_tform_mat_for_cv2
 
 # reference facial points, a list of coordinates (x,y)
 REFERENCE_FACIAL_POINTS = [
@@ -211,7 +211,7 @@ def warp_and_crop_face(src_img, facial_pts, reference_pts=None, crop_size=(96, 1
     ref_pts = np.float32(reference_pts)
     src_pts = np.float32(facial_pts)
 
-    trans, _ = get_similarity_transform(src_pts, ref_pts, True)
+    trans = findSimilarity(src_pts, ref_pts)
     tfm = trans[:, 0:2].T
     face_img = cv2.warpAffine(src_img, tfm, (crop_size[0], crop_size[1]))
     return face_img
--- face_alignment/mtcnn_pytorch/src/matlab_cp2tform.py
+++ face_alignment/mtcnn_pytorch/src/matlab_cp2tform.py
@@ -116,77 +116,28 @@ def findNonreflectiveSimilarity(uv, xy, options=None):
 
 
 def findSimilarity(uv, xy, options=None):
-
     options = {'K': 2}
-
-#    uv = np.array(uv)
-#    xy = np.array(xy)
-
-    # Solve for trans1
-    trans1, trans1_inv = findNonreflectiveSimilarity(uv, xy, options)
-
-    # Solve for trans2
-
-    # manually reflect the xy data across the Y-axis
+    trans1, _ = findNonreflectiveSimilarity(uv, xy, options)
     xyR = xy
     xyR[:, 0] = -1 * xyR[:, 0]
-
-    trans2r, trans2r_inv = findNonreflectiveSimilarity(uv, xyR, options)
-
-    # manually reflect the tform to undo the reflection done on xyR
-    TreflectY = np.array([
-        [-1, 0, 0],
-        [0, 1, 0],
-        [0, 0, 1]
-    ])
-
+    trans2r, _ = findNonreflectiveSimilarity(uv, xyR, options)
+    TreflectY = np.array([[-1, 0, 0], [0, 1, 0], [0, 0, 1]])
     trans2 = np.dot(trans2r, TreflectY)
-
-    # Figure out if trans1 or trans2 is better
     xy1 = tformfwd(trans1, uv)
     norm1 = norm(xy1 - xy)
 
     xy2 = tformfwd(trans2, uv)
     norm2 = norm(xy2 - xy)
 
+    
     if norm1 <= norm2:
-        return trans1, trans1_inv
+        return trans1
     else:
-        trans2_inv = inv(trans2)
-        return trans2, trans2_inv
+        return trans2
 
 
 def get_similarity_transform(src_pts, dst_pts, reflective=True):
-    """
-    Function:
-    ----------
-        Find Similarity Transform Matrix 'trans':
-            u = src_pts[:, 0]
-            v = src_pts[:, 1]
-            x = dst_pts[:, 0]
-            y = dst_pts[:, 1]
-            [x, y, 1] = [u, v, 1] * trans
 
-    Parameters:
-    ----------
-        @src_pts: Kx2 np.array
-            source points, each row is a pair of coordinates (x, y)
-        @dst_pts: Kx2 np.array
-            destination points, each row is a pair of transformed
-            coordinates (x, y)
-        @reflective: True or False
-            if True:
-                use reflective similarity transform
-            else:
-                use non-reflective similarity transform
-
-    Returns:
-    ----------
-       @trans: 3x3 np.array
-            transform matrix from uv to xy
-        trans_inv: 3x3 np.array
-            inverse of trans, transform matrix from xy to uv
-    """
 
     if reflective:
         trans, trans_inv = findSimilarity(src_pts, dst_pts)
@@ -197,28 +148,6 @@ def get_similarity_transform(src_pts, dst_pts, reflective=True):
 
 
 def cvt_tform_mat_for_cv2(trans):
-    """
-    Function:
-    ----------
-        Convert Transform Matrix 'trans' into 'cv2_trans' which could be
-        directly used by cv2.warpAffine():
-            u = src_pts[:, 0]
-            v = src_pts[:, 1]
-            x = dst_pts[:, 0]
-            y = dst_pts[:, 1]
-            [x, y].T = cv_trans * [u, v, 1].T
-
-    Parameters:
-    ----------
-        @trans: 3x3 np.array
-            transform matrix from uv to xy
-
-    Returns:
-    ----------
-        @cv2_trans: 2x3 np.array
-            transform matrix from src_pts to dst_pts, could be directly used
-            for cv2.warpAffine()
-    """
     cv2_trans = trans[:, 0:2].T
 
     return cv2_trans
